8620390405e7580ab011a9c80d19a95242020e4a
eden <xiaoyumqyz@gmail.com>
2021-04-12 12:24:34 +1200

    initial commit

---
+++ apps/google-news-clone/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sirius-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'google-news-clone';
+}
