af1ceb512c42b8f16bed43b9e5a3153165216663
lantisky <lantislexc@gmail.com>
2021-04-12 20:09:32 +1200

    add 3 files

---
+++ apps/google-news-clone/src/app/components/topic-card/topic-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sirius-topic-card',
+  templateUrl: './topic-card.component.html',
+  styleUrls: ['./topic-card.component.scss']
+})
+export class TopicCardComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
