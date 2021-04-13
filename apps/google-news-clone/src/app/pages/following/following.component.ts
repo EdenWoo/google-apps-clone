9fed2463a89ee0cfe961a1fd5279eec1d0fdd31c
lantisky <lantislexc@gmail.com>
2021-04-13 20:33:03 +1200

    fix img

--- apps/google-news-clone/src/app/pages/following/following.component.ts
+++ apps/google-news-clone/src/app/pages/following/following.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FollowingComponent implements OnInit {
 
+  imgList = {
+    name: 'img',
+    fields: [
+      {val: "https://lh3.googleusercontent.com/o_tai07eFNo8w2jfrZY_vh2Mv3DnrgXM1Ven6HBYn4vFxe949KwJgvAhYdq2Hmr4C_5jUbkkn84=-rw"},
+      {val: "https://lh3.googleusercontent.com/7Iv4pkYA_hqsvlyo6XNy3UU0tUYgBR9rGrDHekm8-6cHO14jbUrOu8dCU86to2kzYoRVHJn0Ow=-rw"},
+    ]
+  };
+  saveSearchImg = 'https://lh3.googleusercontent.com/o_tai07eFNo8w2jfrZY_vh2Mv3DnrgXM1Ven6HBYn4vFxe949KwJgvAhYdq2Hmr4C_5jUbkkn84=-rw';
+  saveStoryImg = 'https://lh3.googleusercontent.com/7Iv4pkYA_hqsvlyo6XNy3UU0tUYgBR9rGrDHekm8-6cHO14jbUrOu8dCU86to2kzYoRVHJn0Ow=-rw';
   constructor() { }
 
   ngOnInit(): void {
