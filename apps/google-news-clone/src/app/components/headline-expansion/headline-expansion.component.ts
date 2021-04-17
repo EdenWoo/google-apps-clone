33614c1e6d7e213cdfd62281c6505ef4cb26eaea
eden <xiaoyumqyz@gmail.com>
2021-04-17 20:22:24 +1200

    expansion refactor

--- apps/google-news-clone/src/app/components/headline-expansion/headline-expansion.component.ts
+++ apps/google-news-clone/src/app/components/headline-expansion/headline-expansion.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-headline-expansion',
-  templateUrl: './headline-expansion.component.html',
-  styleUrls: ['./headline-expansion.component.scss']
+  selector: "app-headline-expansion",
+  templateUrl: "./headline-expansion.component.html",
+  styleUrls: ["./headline-expansion.component.scss"]
 })
 export class HeadlineExpansionComponent implements OnInit {
+  collapsed: boolean;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
