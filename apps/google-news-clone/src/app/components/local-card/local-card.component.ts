c8f75fa878d8d173498b5b4797e9565cee8bc680
lantisky <lantislexc@gmail.com>
2021-04-17 23:37:00 +1200

    fix dependence from origin repositories from EdenWoo/google-apps-clone

---
+++ apps/google-news-clone/src/app/components/local-card/local-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-local-card',
+  templateUrl: './local-card.component.html',
+  styleUrls: ['./local-card.component.scss']
+})
+export class LocalCardComponent implements OnInit {
+  cityName = {
+    name: 'city',
+    fields: [
+      {val: "Auckland "},
+      {val: "Hamilton"}
+    ]
+  };
+
+  imgAuckland = 'https://lh5.googleusercontent.com/proxy/fz_nRcvaNxzI8eF2uR0f6qDItPvGAsrRSzBPoWbehf87bgUFHDY4Em8hXS9ESs9BvkMQ9nDH3YHmlqnYoETxDZBL-BQuGRiXixJwdmcj83Hcm2P_jlcH8fDjljgdr-AP03Ln6vmHr-bQLGTQ7h3hJIss9kQJKGlUyRupBC56HiHufzo=p-rw';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apps/google-news-clone/src/app/components/top-news/top-news.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-news',
+  templateUrl: './top-news.component.html',
+  styleUrls: ['./top-news.component.scss']
+})
+export class TopNewsComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
