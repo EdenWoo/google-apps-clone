3ce5652354df85dd6942b8f06d0fb30fef64eeed
lantisky <lantislexc@gmail.com>
2021-04-19 12:44:20 +1200

    Add nav lists

--- apps/google-news-clone/src/app/components/side-menu/side-menu.component.ts
+++ apps/google-news-clone/src/app/components/side-menu/side-menu.component.ts
@@ -21,6 +21,15 @@ export class SideMenuComponent {
     {name: 'Following', link: 'following', icon: 'star_border'},
     {name: 'Saved searched', link: 'saved-search', icon: 'search'},
     {name: 'Covid-19', link: 'covid-19', icon: 'shield'},
+    {name: 'New Zealand', link: 'new-zealand', icon: 'flag'},
+    {name: 'World', link: 'world', icon: 'public'},
+    {name: 'Your local news', link: 'local-news', icon: 'location_on'},
+    {name: 'Business', link: 'business', icon: 'business'},
+    {name: 'Technology', link: 'technology', icon: 'memory'},
+    {name: 'Entertainment', link: 'entertainment', icon: 'theaters'},
+    {name: 'Sports', link: 'sports', icon: 'directions_bike'},
+    {name: 'Science', link: 'science', icon: 'science'},
+    {name: 'Health', link: 'health', icon: 'health_and_safety'},
   ];
 
   isMobile$ = this.breakpointObserver.observe(`(max-width: ${SCREEN_SM_MAX}px)`).pipe(map(breakpoint => breakpoint.matches));
